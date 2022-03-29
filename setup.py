6f8f2c676b26daf0c471f30faccf510f4741a605
jean.demeusy <jean.demeusy@master.hes-so.ch>
2022-03-29 16:31:50 +0200

    updated package name

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-    name="network-visualizer",
+    name="neural-network-renderer",
     version="0.1",
-    scripts=["bin/network-visualizer"],
+    scripts=["bin/neural-network-renderer"],
     author="Jean Demeusy",
     author_email="dev.jdu@gmail.com",
     description="A usefull CNN/DenseNet visualization tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jeandemeusy/network-visualizer",
-    packages=["network-visualizer"],
+    url="https://github.com/jeandemeusy/neural-network-renderer",
+    packages=["neural-network-renderer"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
